1f564b800e2e8f141ba34063b0057cecd7d16a76
yb <scauyb@qq.com>
2024-08-03 18:23:17 +0800

    feat(popup):集成词汇本组件并更新项目版本
    将词汇本组件集成到popup/App.tsx中，移除原有的WXT和React标志以及计数器按钮，简化为直接渲染词汇本组件。
    同时，更新项目版本号至0.1.7，修改LICENSE文件中的许可证信息，并修正了startTrans.ts中的Promise实现。

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,54 +1,10 @@
-import reactLogo from '@/assets/react.svg'
-import { useState } from 'react'
-import wxtLogo from '/wxt.svg'
 import './App.css'
+import { VocabularyBook } from '@/entrypoints/popup/popup-main/VocabularyBook.tsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div className={'bg-amber'}>
-        <a
-          href='https://wxt.dev'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img
-            src={wxtLogo}
-            className='logo'
-            alt='WXT logo'
-          />
-        </a>
-        <a
-          href='https://react.dev'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img
-            src={reactLogo}
-            className='logo react'
-            alt='React logo'
-          />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className='card'>
-        <button
-          type='button'
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the WXT and React logos to learn more
-      </p>
+      <VocabularyBook />
     </>
   )
 }
-
-export default App
