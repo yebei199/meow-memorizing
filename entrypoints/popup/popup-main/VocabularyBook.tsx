c2d7fd5d50e1208bd1f9ae4c4c947bacc0f8e677
yb <scauyb@qq.com>
2024-08-04 02:23:10 +0800

    feat:新增初始单词本,popup主页面添加词汇书功能以及trans.content中的删除次数更新"

--- entrypoints/popup/popup-main/VocabularyBook.tsx
+++ entrypoints/popup/popup-main/VocabularyBook.tsx
@@ -1,14 +1,38 @@
+import { getWordsList } from '@/entrypoints/trans.content/script/storageAction.ts'
 import type {
   IALlWordsStorage,
   IWordStorage,
 } from '@/src/wxtStore.ts'
+import type { TableColumnsType } from 'antd'
+import { Space, Table, Tag } from 'antd'
+import type { TableProps } from 'antd'
+import { useEffect } from 'react'
 
 export const VocabularyBook = () => {
-  const [words, setWords] = useState<IWordStorage[]>([])
+  console.log('VocabularyBook')
+  const defaultWords: IALlWordsStorage = {}
+
+  const [words, setWords] =
+    useState<IALlWordsStorage>(defaultWords)
+
+  useEffect(() => {
+    async function fetchWords() {
+      try {
+        const words1 = await getWordsList()
+        if (words1) {
+          setWords(words1)
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    fetchWords().catch(console.error)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setWords])
+
   return (
     <div>
-      <h2>Vocabulary Book</h2>
-      <p>这是啥</p>
+      <Sheet wordsList={words} />
     </div>
   )
 }
@@ -16,23 +40,70 @@ export const VocabularyBook = () => {
 function Sheet({
   wordsList,
 }: { wordsList: IALlWordsStorage }) {
-  return <div>{}</div>
+  const [showWords, setShowWords] = useState<IShowWord[]>(
+    [],
+  )
+  console.log('wordsList', wordsList)
+  useEffect(() => {
+    const showWords1 = Object.keys(wordsList).map((i) => {
+      return {
+        key: i,
+        word: wordsList[i].word,
+        queryTimes: wordsList[i].queryTimes,
+        deleteTimes: wordsList[i].deleteTimes,
+      }
+    })
+    setShowWords(showWords1)
+  }, [wordsList, setShowWords])
+
+  return (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={showWords}
+        pagination={{ pageSize: 5 }}
+        className={'custom-row-10'}
+        rowClassName='h-0.01'
+      />
+    </div>
+  )
 }
 
-const columns = [
+export interface IShowWord {
+  key: string
+  word: string
+  queryTimes: number
+  deleteTimes: number
+}
+
+/*
+ * ant table 单词列表的列接口
+ */
+export interface IColumn {
+  title: string
+  dataIndex: string
+  key: string
+  width?: number
+}
+
+const columns: TableColumnsType<IShowWord> = [
   {
     title: '单词',
+    width: 10,
+
     dataIndex: 'word',
     key: 'word',
   },
   {
     title: '查询次数',
+    width: 60,
     dataIndex: 'queryTimes',
     key: 'queryTimes',
   },
   {
     title: '删除次数',
-    dataIndex: 'address',
-    key: 'address',
+    width: 60,
+    dataIndex: 'deleteTimes',
+    key: 'deleteTimes',
   },
 ]
